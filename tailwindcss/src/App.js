6a6d5abab562e96c242662409c6f219cf089aa9a
bsaranga <bsaranga@gmail.com>
2022-11-28 09:00:08 +0530

    widths

--- tailwindcss/src/App.js
+++ tailwindcss/src/App.js
@@ -1,7 +1,12 @@
 function App() {
   return (
-    <div className="container mx-auto debug">
-      <div className="text-xl">Hello World</div>
+    <div className="container mx-auto">
+      <div className="flex debug min-w-max">
+        <div className="text-xl debug">Home</div>
+        <div className="text-xl basis-2/5 debug">Nav 1</div>
+        <div className="text-xl debug">Nav 2</div>
+        <div className="text-xl debug">Nav 3</div>
+      </div>
     </div>
   );
 }
